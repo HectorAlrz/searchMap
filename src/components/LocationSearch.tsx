83d8fa5be747427656f5f4a9b5d432162d99cadc
HectorAlrz <healcaraz@gmail.com>
2024-10-24 15:15:42 -0600

    feat: add svg and input to locationsearch

--- src/components/LocationSearch.tsx
+++ src/components/LocationSearch.tsx
@@ -6,9 +6,29 @@ interface LocationSearchProps {
 }
 
 function LocationSearch({ onPlaceClick }: LocationSearchProps) {
+  const [term, setTerm] = useState<string>("");
   const [places, setPlaces] = useState<Place[]>([]);
 
-  return <div>LocationSearch</div>;
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label className="font-bold" htmlFor="term">
+          Search
+        </label>
+        <input
+          className="border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 px-4 py-2 w-full"
+          id="term"
+          placeholder="Search in map"
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+        />
+      </form>
+    </div>
+  );
 }
 
 export default LocationSearch;
